cf5bd5abfd1386a29bf4ab02c3b01104fe74bb35
lumingfu <lumingfu@bonc.com.cn>
2022-07-22 07:52:55 +0800

    修改公众号链接

--- docs/.vuepress/htmlModules.js
+++ docs/.vuepress/htmlModules.js
@@ -55,7 +55,7 @@ module.exports = {
       margin: 0 0 8px 0;
     ">公众号</p>
     <img src="./img/architect.jpg"  style="width:100%;" />
-    关注公众号，回复[<b>666</b>]，可获取 <a href="https://game.xugaoyi.com" arget="_blank" >福利干货资源<span><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg> <span class="sr-only">(opens new window)</span></span></a>
+    关注公众号，回复[<b>666</b>]，可获取 <a href="https://www.javaessay.cn/pages/a30957/" arget="_blank" >福利干货资源<span><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg> <span class="sr-only">(opens new window)</span></span></a>
     </p>
     </div>`,
     // windowRB: `
