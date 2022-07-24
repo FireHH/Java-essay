5ae70a9f085b5524d877bb9e22c7a6fb5ca65ab6
lumingfu <lumingfu@bonc.com.cn>
2022-07-24 22:21:16 +0800

    github自动部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,7 +38,7 @@ module.exports = {
     blogger: {
       avatar: '/img/huge.jpg',
       name: 'Mr.Fire',
-      slogan: '后端',
+      slogan: '后端程序员一枚',
     },
 
     // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
