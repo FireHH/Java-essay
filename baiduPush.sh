45ca2564ee31ca521516671e05d67452323dd07a
FireHH <m18388461281@163.com>
2022-11-12 22:08:41 +0800

    百度推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://javaessay.cn&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.javaessay.cn&token=BWTfCdO6SyG46mrq"
 
 rm -rf urls.txt # 删除文件
