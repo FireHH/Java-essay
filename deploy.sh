50bb2c55fdb7bbd90d705f018be479491650d6d2
FireHH <m18388461281@163.com>
2022-11-12 12:26:04 +0800

    修改提交

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,12 @@ cd $dist_path
 #发布到自定义域名
 echo 'javaessay.cn' > CNAME
 
+git config --global user.email "m18388461281@163.com"
+git config --global user.name "FireHH"
+
 git init
 git add -A
+
 git commit -m "deploy, $commit_info"
 git push -f $push_addr HEAD:$push_branch
 
