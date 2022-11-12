2cbbf13a1bb3dae9307c06d87a5f823b9ba79a04
Mr.Fire <m18388461281@163.com>
2022-11-12 09:48:13 +0800

    修改提交

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,25 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+push_addr=git@github.com:FireHH/firehh.github.io.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=master # 推送的分支
+
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd $dist_path
 
 #发布到自定义域名
 echo 'javaessay.cn' > CNAME
 
 git init
 git add -A
-git commit -m 'message'
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
-git config --global user.email "m18388461281@163.com"
-git config --global user.name "FireHH"
-
-# 如果发布到 https://<FireHH>.github.io
-#git remote add origin git@github.com:FireHH/Java-essay.git
-git push -f git@github.com:FireHH/firehh.github.io.git master
-
-cd ../ && rm -rf dist
+cd -
+rm -rf $dist_path
