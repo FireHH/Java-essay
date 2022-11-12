93c70f1d224aef55882d07c320c4104ecd1dbcbe
FireHH <m18388461281@163.com>
2022-11-12 16:20:20 +0800

    修改地址

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 push_addr=git@github.com:FireHH/Java-essay.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+deploy_branch=gh-pages # 推送的分支
 
 # 生成静态文件
 npm run build
@@ -24,7 +24,9 @@ git config --global user.name "FireHH"
 git add -A
 
 git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+remote_addr=https://FireHH:${{secrets.ACCESS_TOKEN}}@${remote_addr}
+git remote add origin ${remote_addr}
+git push origin HEAD:$deploy_branch --force # 推送到github $deploy_branch分支
 
 cd -
 rm -rf $dist_path
